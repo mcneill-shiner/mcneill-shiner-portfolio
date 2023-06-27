8619e45f6b285f8a97120b63258994e18ed81d24
McNeill Shiner <mcneill.shiner@gmail.com>
2023-06-26 20:23:58 -0400

    tailwind install, config

---
+++ .eleventy.js
@@ -0,0 +1,38 @@
+const { DateTime } = require('luxon');
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('src/assets/');
+
+    // eleventyConfig.addCollection('posts', function(collectionApi) {
+    //     return collectionApi.getFilteredByGlob('src/blog/posts/**/*.md')
+    // })
+
+    // // to format dates
+    // eleventyConfig.addFilter('postDate', (dateObj) => {
+    //     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    // });
+
+    // // to create excerpts
+    // eleventyConfig.setFrontMatterParsingOptions({
+    //     excerpt: true,
+    //     excerpt_alias: 'post_excerpt',
+    //     excerpt_separator: '<!-- excerpt -->'
+    // })
+
+    // // asset img shortcode
+    // eleventyConfig.addNunjucksShortcode('asset_img', (filename, alt) => {
+    //     return `<img class="" src="/assets/img/posts/${filename}" alt="${alt}" />`
+    // })
+
+    return {
+    dir: {
+        input: 'src',
+        includes: '_includes',
+        output: '_site',
+    },
+    templateFormats: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    };
+}
