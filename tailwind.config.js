167cc49941db95d3f7400c113b5961df8f95b623
McNeill Shiner <mcneill.shiner@gmail.com>
2023-06-30 20:46:07 -0400

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./_site/**/*.{html,js,njk}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'kombu': '414400',
+        'olive': '626c25',
+        'lemon': 'f0d876',
+        'delft': '4472b8',
+        'almond': 'f7f6ef'
+      },
+      fonts: {
+        'sans': ['"Space Mono"','"Trebuchet MS"', 'ui-sans-serif'],
+        'serif': ['BioRhyme', '"Courier New"', 'ui-system-serif']
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography')
