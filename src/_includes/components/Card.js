d1b4b41b82ab02dddc57ed99c1ab75964f912417
McNeill Shiner <mcneill.shiner@gmail.com>
2023-07-08 14:43:54 -0400

    edits to card component

--- src/_includes/components/Card.js
+++ src/_includes/components/Card.js
@@ -4,16 +4,16 @@ function Card({title, img, imgAlt, stack, desc, url, gitUrl}) {
     stack.map(el => `<li>${el}</li>`).join('')
 
     return html`
-        <div>
-            <img src="${img}" alt="${imgAlt}">
+        <div class="py-4">
+            <a href="${url}" target="_blank"><img src="${img}" alt="${imgAlt}"></a>
             <span class="block font-semibold text-xl py-4">${title}</span>
             <ul class="uppercase font-serif flex gap-2">
                     ${stack}
             </ul>
             <p class="py-4">${desc}</p>
-            <div class="flex gap-4 uppercase font-serif underline underline-offset-8 decoration-olive">
+            <div class="flex gap-4 uppercase font-serif underline underline-offset-8 decoration-2 decoration-olive">
                 <span><a href="${url}" target="_blank">View Project</a></span>
-                <span><a href="${gitUrl}" target="_blank">View Code</a></span>
+                ${gitUrl ? `<span><a href="${gitUrl}" target="_blank">View Code</a></span>` : ``}
             </div>
 
         </div>
