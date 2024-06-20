316552a34935d4f9a419ef1447bb4681adbf3c5f
mcneill-shiner <mcneill.shiner@gmail.com>
2024-06-20 12:47:07 -0600

    Updated font size

--- src/_includes/components/Button.js
+++ src/_includes/components/Button.js
@@ -2,7 +2,7 @@ module.exports = (text, color, link, isSubmit, id) => {
   if (isSubmit) {
     return `
       <div>
-        <button id=${id} class="bg-${color} text-xl font-medium uppercase p-2 border-2 border-black rounded-lg">
+        <button id=${id} class="bg-${color} text-lg font-medium uppercase p-2 border-2 border-black rounded-lg">
           <span
             class=""
             >${text}</span>
@@ -14,7 +14,7 @@ module.exports = (text, color, link, isSubmit, id) => {
     <div>
     <a href="${link}"
       ><span
-        class="bg-${color} text-xl font-medium uppercase p-2 border-2 border-black rounded-lg"
+        class="bg-${color} text-lg font-medium uppercase p-2 border-2 border-black rounded-lg"
         >${text}</span
       ></a
     >
