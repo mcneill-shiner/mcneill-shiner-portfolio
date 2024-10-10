9cb596be0a7e7a2251f7b0df438ba215659722b6
mcneill-shiner <mcneill.shiner@gmail.com>
2024-10-10 16:16:27 -0600

    update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'kombu': '#626c25',
+        'kombu': '#5A5007',
         'almond': '#f7f6ef',
         'kraft': '#c0a377',
         'pear': '#bbb45b',
         'cameo': '#e7a491',
-        'sunshine': '#e1c667',
-        'oxblood': '#310101'
+        'sunshine': '#e4c34e',
+        'oxblood': '#310101',
+        'lapis': '#364DA1'
       },
       fontFamily: {
         'sans': ['"Inter"','"Trebuchet MS"', 'ui-sans-serif'],
