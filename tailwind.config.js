7ecc30a4e0ab28f7256c89e608b24e26880471df
mcneill-shiner <mcneill.shiner@gmail.com>
2024-10-12 10:24:14 -0600

    added crimson color, removed cameo

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,10 @@ module.exports = {
         'almond': '#f7f6ef',
         'kraft': '#c0a377',
         'pear': '#bbb45b',
-        'cameo': '#e7a491',
         'sunshine': '#e4c34e',
         'oxblood': '#310101',
-        'lapis': '#364DA1'
+        'lapis': '#364DA1',
+        'crimson': '#B9200A'
       },
       fontFamily: {
         'sans': ['"Inter"','"Trebuchet MS"', 'ui-sans-serif'],
